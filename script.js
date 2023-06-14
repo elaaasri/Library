4fd3fed191bc1e11395cf144f2974648c2933426
elaaasri <anas_elasri@outlook.fr>
2023-06-14 21:42:44 +0100

    unfinished script 2

--- script.js
+++ script.js
@@ -1,106 +1,88 @@
-let userInput = document.querySelector(".user-input");
-let submit = document.querySelector("#submit-form");
-// let form = document.querySelector("#form");
 let newBookButton = document.querySelector(".new-book");
 let bookForm = document.querySelector("#book-form")
+let authorInput = document.querySelector("#author-input");
+let titleInput = document.querySelector("#title-input");
+let numOfPagesInput = document.querySelector("#number-input");
 
 let myLibrary = [];
-
-console.log(newBookButton);
-
-function addBookToLibrary() {
-    
-    //  takes the user input and store it as an object in new array :
-    submit.addEventListener("submit", function () {
-
-        let userValue = userInput.value;
-        myLibrary.push({userValue});
-        
-        console.log(myLibrary);
-        console.log(typeof myLibrary);
-        console.log(myLibrary[0] instanceof Object);
-
-        displayEachBook();
-        
-    });
-};
-addBookToLibrary();
-
-function Book() {
-    // the constructor...
-
-    }
-
-// myLibrary = [{userValue : "anas"}, {userValue : "zbe"},{userValue : "aa"}]
-
-function displayEachBook () {
-
-    for (let book in myLibrary) {
-        
-        let showBook = document.createElement("div");
-        showBook.classList = "show-book";
-        showBook.textContent = myLibrary[book].userValue;
-        document.body.appendChild(showBook);
-        
-    };
-};
-displayEachBook();
+let myObj = {};
 
 function Book(author, title, numberOfPages) {
     // the constructor...
     this.author = author;
     this.title = title;
     this.numberOfPages = numberOfPages;
-    };
-
+};
 
 newBookButton.addEventListener("click", function() {
-    console.log("clicked");
     bookForm.style.cssText = 'display; block';
 });
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-// newBookButton.addEventListener("click",function () {
-//     let newForm = document.createElement("form");
-//     newForm.classList = "new-form";
-//     newForm.textContent = "Add New Book";
-//     document.body.appendChild(newForm);
-
-//     let author = document.createElement("input");
-//     author.setAttribute("placeholder", "author");
-//     newForm.appendChild(author)
-
-//     let title = document.createElement("input");
-//     title.setAttribute("placeholder", "title");
-//     newForm.appendChild(title);
+function addBookToLibrary() {
     
-//     let numberOfPages = document.createElement("input");
-//     numberOfPages.setAttribute("placeholder", "numberOfPages");
-//     newForm.appendChild(numberOfPages);
-// });
+    //  takes the user input and store it as an object in new array :
+    bookForm.addEventListener("submit", function (event) {
+        let authorValue = authorInput.value;
+        let titleValue = titleInput.value;
+        let numberOfPagesValue = numOfPagesInput.value;
+
+        myObj = new Book(authorValue, titleValue, numberOfPagesValue);
+        myLibrary.push(myObj);
+
+        displayEachBook();
+        event.preventDefault();
+        
+    });
+};
+addBookToLibrary();
+
+// myLibrary = [{value : "zbe"},{value : "a"}];
+
+function displayEachBook () {
+    
+    for (let book in myLibrary) {
+        // a condition to check if library is instanceof the constructor and if it has a length (condition not necessary!) :
+        if (myLibrary[book] instanceof Book && myLibrary.length) {
+            
+                // creating new elements : 
+            let showBook = document.createElement("div");
+            let authorOutput = document.createElement("div");
+            let titleOutput = document.createElement("div");
+            let numberOfPagesOutput = document.createElement("div");
+            let removeBookButton = document.createElement("button");
+
+            //  setting classlist of the new elements : 
+            showBook.classList = "show-book";
+            authorOutput.classList = "author-output";
+            titleOutput.classList = "title-output";
+            numberOfPagesOutput.classList = "author-output";
+            removeBookButton.classList = "remove-button";
+            
+            //  adding text content : 
+            authorOutput.textContent = myLibrary[book].author;
+            titleOutput.textContent = myLibrary[book].title; 
+            numberOfPagesOutput.textContent = myLibrary[book].numberOfPages;
+            removeBookButton.textContent = "remove";
+            
+            // appending elements :
+            document.body.appendChild(showBook);
+            showBook.appendChild(authorOutput);
+            showBook.appendChild(titleOutput);
+            showBook.appendChild(numberOfPagesOutput);
+            showBook.appendChild(removeBookButton);
+            
+            myLibrary.shift();
+            
+                // event button to remove the current book :
+            removeBookButton.addEventListener("click", function() {
+                showBook.remove();
+            });
+
+        
+        };
+    };
+};
+displayEachBook();
 
 
 
@@ -122,31 +104,10 @@ newBookButton.addEventListener("click", function() {
 
 
 
-    //  inside the addBookToLibrary to show every book on the page!
-            // display each book func : 
-        // function displayEachBook () {
-        //     let result = document.createElement("div");
-        //     result.classList = "result";
-        //     document.body.appendChild(result);
-
-        //     for (let books in myLibrary) {
-        //         result.textContent = myLibrary[books].userValue;
-                
-        //         myLibrary[books].userValue === "" ? console.log("zbe") : "";
-                
-
-        //     };
-        // };
-        // displayEachBook();*
 
 
 
-
-
-
-
-
-
+        
 
 
 
